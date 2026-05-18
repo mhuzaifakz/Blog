c7f6e5396847dbb3acdd3c4fba4f3f7391197e1b
Muhammad Huzaifa <m.huzaifa.khandaza@icloud.com>
2026-05-18 22:54:59 +0500

    not thing done rigth now

--- javascript/api.js
+++ javascript/api.js
@@ -7,3 +7,13 @@ export async function getSingleNews (id) {
     const fetching = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}` )
     return fetching.json()
 }
+
+export async function getPostUser (id) {
+    const fetching = await fetch(`https://jsonplaceholder.typicode.com/users/${id}` )
+    return fetching.json()
+}
+
+export async function getPostComment (id) {
+    const fetching = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}/comments` )
+    return fetching.json()
+}
